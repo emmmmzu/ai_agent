5986a81eb23a01c5ce72789be6e278feb9e4fff1
emmmmzu <emma.reinbold@web.de>
2025-06-21 17:58:53 +0200

    verbose things shouldn't always be printed

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    args = sys.argv[1:]
+    
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}")
     
     messages = [
         types.Content(role="user",parts=[types.Part(text=user_prompt)]),
@@ -25,9 +30,12 @@ def main():
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
