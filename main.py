81a391ee17e32f6193c41a9741c11753cd9b8526
emmmmzu <emma.reinbold@web.de>
2025-06-21 17:43:47 +0200

    added storing of conversation

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
     args = sys.argv[1:]
 
     if not args:
@@ -16,12 +16,15 @@ def main():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
-
+    
+    messages = [
+        types.Content(role="user",parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
-        )
+        contents=messages,
+    )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
