aacd15227305df68f1fabef2712a8ef8a323dc32
emmmmzu <emma.reinbold@web.de>
2025-07-03 06:34:33 +0200

    making the ai use the functions constrained to the working directory

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,7 @@
 import os
+from google.genai import types
 
+MAX_CHARS = 10000
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -9,17 +11,33 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(target_file):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
-    max_chars = 10000
-    too_long = max_chars + 1
+
+    too_long = MAX_CHARS + 1
 
     try:
         with open(target_file, "r") as f:
-            file_content_string = f.read(max_chars)
+            file_content_string = f.read(MAX_CHARS)
         with open(target_file, "r") as f:
             truncate = f.read(too_long)
-        if len(truncate) > max_chars:
+        if len(truncate) > MAX_CHARS:
             truncated = str(file_content_string) + '\n[...File "{file_path}" truncated at 10000 characters]'
             return truncated
         return file_content_string
     except Exception as e:
         return f"Error opening file: {e}"
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def write_file(working_directory, file_path, content):
@@ -6,6 +7,39 @@ def write_file(working_directory, file_path, content):
     target_file = os.path.abspath(os.path.join(working_directory, file_path))
     if not target_file.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    with open(target_file, "w") as f:
-        f.write(content)
-    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    if not os.path.exists(target_file):
+        try:
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(target_file) and os.path.isdir(target_file):
+        return f'Error: "{file_path}" is a directory, not a file'
+    try:
+        with open(target_file, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error: writing to file: {e}"
+
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes content to a file within the working directory. Creates the file if it doesn't exist.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write to the file",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
